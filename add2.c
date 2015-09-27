6fd860d2ba50f73476fa2721ee02045328b75080
Luc Libralesso <libralesso.l@gmail.com>
2015-09-27 17:18:08 -0400

    correct style

--- add2.c
+++ add2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-int add_thread(int* t, int bi, int bs){
+int addThread(int* t, int bi, int bs){
 	int i, res=0;
 	for ( i = bi ; i <= bs ; i++ )
 		res += t[i];
@@ -10,33 +10,33 @@ int add_thread(int* t, int bi, int bs){
 
 int add(int* t, int len){
 	
-	int nb_threads = sqrt(len);
-	int res_threads[nb_threads]; int res_main = 0;
-	int step = len/nb_threads;
+	int nbThreads = sqrt(len);
+	int resThreads[nbThreads]; int resMain = 0;
+	int step = len/nbThreads;
 	int i, pos = 0;
 	
 	// section 1
 	// spawning threads
-	// parallel for ( i = 0 ; i < nb_threads ; i++){
-	for ( i = 0 ; i < nb_threads ; i++){
-		res_threads[i] = add_thread(t, pos, pos+step-1);
-		printf("res thread: %d\n", res_threads[i]);
+	// parallel for ( i = 0 ; i < nbThreads ; i++){
+	for ( i = 0 ; i < nbThreads ; i++){
+		resThreads[i] = addThread(t, pos, pos+step-1);
+		printf("res thread: %d\n", resThreads[i]);
 		pos += step;
 	}
 	
 	// section 2
 	// main thread coup
 	for ( i = pos ; i < len ; i++ ){
-		res_main += t[i];
+		resMain += t[i];
 	}
 	
 	// section 3
 	// sync
 	// computing result 
-	for ( i = 0; i < nb_threads; i++ )
-		res_main += res_threads[i];
+	for ( i = 0; i < nbThreads; i++ )
+		resMain += resThreads[i];
 	
-	return res_main;
+	return resMain;
 } 
 
 int main(){
